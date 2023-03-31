4531f5a3296789a0294de81228ae5e8a734d43d7
Diego Duré <robertti_diego_@hotmail.com>
2023-03-31 19:05:13 -0400

    v1.1

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const app = express();
 app.use(bodyParser());
 const userRoute = require("./src/route/users/users.route");
 const themeRoute = require("./src/route/themes/themes.route");
+const themePropertyRoute = require("./src/route/themesProperties/themesProperties.route");
 const topicRoute = require("./src/route/topics/topics.route");
 //Ruta raiz
 app.get('/', function (req, res) {
-    //Logica.
     res.send('Hello World');
 });
 
 app.get('/pagina2', function (req, res) {
-    //Logica de negocios
-    //esta aqui -Controller
-
     res.json({ application: 'Study APP', version: '1.0.0' });
 });
 
-//Llamadas a los routes de los UCs
+//Llamadas a los routes
 userRoute(app);
 themeRoute(app);
+themePropertyRoute(app);
 topicRoute(app);
 
 app.listen(3000);
